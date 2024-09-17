8a524cdea4076bb491cd09b4a568eb3eef6aebea
fktakase <fabio.takase@mackenzie.br>
2024-09-16 22:00:52 -0300

    Create exercicio1.c

---
+++ exercicio1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    bool b = false;
+
+   float a;
+
+    scanf("%f", &a);
+    if (a == a){
+       b = true;    
+       printf("%d",b);
+    }
+
+    return 0;
+}
